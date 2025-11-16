3d9ade3716590817f47bd856cfb5855313e4d589
fjohanssonnn <fjohansson.creative@gmail.com>
2025-11-16 14:02:22 +0100

    fix: accidentally removed cn-function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge"
 import type { MenuItem, MenuItemDish } from "@/types";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 export function parseMenuMarkdown(markdown: string): MenuItem[] {
   const lines = markdown.split('\n').filter(line => line.trim());
